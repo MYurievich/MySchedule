e52b13e177c3d6def3f671d441d7b8bc6eea18b2
Maxim <mygordeev@mail.ru>
2021-07-17 15:43:32 +0300

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Максим on 17.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
