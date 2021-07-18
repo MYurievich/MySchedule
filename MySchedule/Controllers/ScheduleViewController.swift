2d79a7c6736185b6fdeec6e345e353194aa10b0a
Maxim <mygordeev@mail.ru>
2021-07-18 17:52:18 +0300

    schedule setCalendar

--- MySchedule/Controllers/ScheduleViewController.swift
+++ MySchedule/Controllers/ScheduleViewController.swift
@@ -6,11 +6,113 @@
 //
 
 import UIKit
+import FSCalendar
 
 class ScheduleViewController: UIViewController {
     
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    let showHideButton: UIButton = {
+       let showHideButton = UIButton()
+        showHideButton.setTitle("Open calendar", for: .normal)
+        showHideButton.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        showHideButton.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        
+        showHideButton.translatesAutoresizingMaskIntoConstraints = false
+        return showHideButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        view.backgroundColor = .white
+        title = "Schedule"
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        
+        calendar.scope = .week
+        
+        setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func showHideButtonTapped() {
+        
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calendar", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calendar", for: .normal)
+        }
+    }
+
+//MARK: SwipeGestureRecognizer
+
+    func swipeAction() {
+    
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+    }
+}
+//MARK: FSCalendarDataSource, FSCalendarDelegate
+
+extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+}
+
+//MARK: SetConstraints
+
+extension ScheduleViewController {
+    
+    func setConstraints () {
+    
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ])
+        
+        view.addSubview(showHideButton)
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
     }
 }
